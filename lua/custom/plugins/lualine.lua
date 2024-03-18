7c92bc8473d09068dbaa4ca7e37dd57cf16469a2
Kiyoshi Shinya <kshinya@buk.cl>
2024-03-18 00:15:26 -0300

    feat: changed lualine style

--- lua/custom/plugins/lualine.lua
+++ lua/custom/plugins/lualine.lua
@@ -1,9 +1,26 @@
 return {
   'nvim-lualine/lualine.nvim',
-  dependencies = { 'nvim-tree/nvim-web-devicons' },
   opts = {
     options = {
-      theme = 'auto',
+      icons_enabled = true,
+      theme = 'horizon',
+      component_separators = '',
+      -- section_separators = '',
+      always_divide_middle = true,
+      globalstatus = true,
+      refresh = {
+        statusline = 1000,
+        tabline = 1000,
+      },
     },
+    sections = {
+      lualine_a = { 'mode' },
+      lualine_b = { 'branch', 'diff' },
+      lualine_c = { 'filename', 'diagnostics' },
+      lualine_x = { 'filetype' },
+      lualine_y = { 'progress' },
+      lualine_z = { 'location' },
+    },
+    extensions = { 'neo-tree' },
   },
 }
